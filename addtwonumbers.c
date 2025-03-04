65ceffb65a672d362d397fe0970cc190c6702bdc
Harwinder <harwinder1340.becse24@chitkara.edu.in>
2025-03-04 17:15:15 +0530

    Add files via upload

---
+++ addtwonumbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter the first value:");
+    scanf("%d",&a);
+    printf("\nEnter the second value:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("Sum of the two numbers is: %d",sum);
+}
